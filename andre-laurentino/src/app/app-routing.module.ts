4751118f816a754c256f73c61f4740daa42d753a
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-08-06 14:43:07 -0300

    feat: adicionado ícones, ajustes de blog, textos de artigo e ajustes página 404

--- andre-laurentino/src/app/app-routing.module.ts
+++ andre-laurentino/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { ErroPageComponent } from './views/erro-page/erro-page.component';
 import { SobreMimComponent } from './views/sobre-mim/sobre-mim.component';
+import { PostComponent } from './views/post/post.component';
+import { BlogComponent } from './views/blog/blog.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'sobre-mim', component:  SobreMimComponent},
+  { path: 'blog', component:  BlogComponent},
+  { path: 'post/:slug', component: PostComponent },
   { path: '**', component: ErroPageComponent }
 ];
 
--- andre-laurentino/src/app/shared/shared.module.ts
+++ andre-laurentino/src/app/shared/shared.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CardBlogComponent } from './components/card-blog/card-blog.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MenuModule } from './components/menu/menu.module';
 import { CardIconsComponent } from './components/card-icons/card-icons.component';
+import { CardBlogModule } from './components/card-blog/card-blog.module';
 
 const sharedComponents = [
-  CardBlogComponent,
   BannerComponent,
   FooterComponent,
   CardIconsComponent
 ]
 
 const sharedModules = [
-  MenuModule
+  MenuModule,
+  CardBlogModule
 ]
 
 @NgModule({
