e6314bb90047c8e0a994b0f8a453e0b6a33cf3f7
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-08-08 13:10:00 -0300

    fix: correcoes visuais do editor

--- andre-laurentino/src/app/views/post/post.component.ts
+++ andre-laurentino/src/app/views/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from 'src/app/shared/services/post.service';
 
@@ -7,7 +7,7 @@ import { PostService } from 'src/app/shared/services/post.service';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   post: any;
   formattedContent: string = '';
 
@@ -21,7 +21,7 @@ export class PostComponent {
     this.postService.getPosts().subscribe(data => {
       this.post = data.find((p: any) => p.slug === slug);
       if (this.post) {
-        this.formattedContent = this.formatContent(this.post.content);
+        this.formattedContent = this.highlightCode(this.formatContent(this.post.content));
       }
     });
   }
@@ -51,19 +51,16 @@ export class PostComponent {
         const tag = headingMap[prefix];
         const tagName = tag.slice(1, -1);
         formattedContent += `${tag}${line.substring(prefix.length)}</${tagName}>\n`;
-      }
-      else if (/^\d+\. /.test(line)) {
+      } else if (/^\d+\. /.test(line)) {
         if (!insideList) {
           formattedContent += '<ol>\n';
           insideList = true;
         }
         listItems.push(`<li>${line.substring(line.indexOf(' ') + 1)}</li>`);
-      }
-      else if (line.startsWith('**') && line.endsWith('**')) {
+      } else if (line.startsWith('**') && line.endsWith('**')) {
         const contentWithoutStars = line.slice(2, -2);
         formattedContent += `<h5>${contentWithoutStars}</h5>\n`;
-      }
-      else {
+      } else {
         if (insideList) {
           formattedContent += listItems.join('\n') + '\n</ol>\n';
           listItems = [];
@@ -78,5 +75,23 @@ export class PostComponent {
     }
 
     return formattedContent.trim();
+  }
+
+  // Editor
+  highlightCode(content: string): string {
+    return content.replace(/<code>(.*?)<\/code>/gs, (match, p1) => {
+      const lines = p1.split('\n').map((line: string, index: number) => {
+        return `<span class="line"><span class="syntax">${this.highlightSyntax(line)}</span></span>`;
+      }).join('\n');
+      return `<div class="code-editor"><pre>${lines}</pre></div>`;
+    });
+  }
+
+  highlightSyntax(line: string): string {
+    line = line.replace(/(const|let|var|function|return)/g, '<span class="keyword">$1</span>');
+    line = line.replace(/('.*?'|".*?"|`.*?`)/g, '<span class="string">$1</span>');
+    line = line.replace(/(\/\/.*?$)/gm, '<span class="comment">$1</span>');
+    line = line.replace(/(\b\d+\b)/g, '<span class="number">$1</span>');
+    return line;
   }
 }
